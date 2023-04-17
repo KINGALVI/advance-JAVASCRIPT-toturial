273b7e7562fd607079cb45b254b1d9a7b5dbd1f1
KINGALVI <alvialmahamud@gmail.com>
2023-04-18 03:15:36 +0600

    edit some code

--- advance-JAVASCRIPT-toturial.js
+++ advance-JAVASCRIPT-toturial.js
@@ -1,4 +1,4 @@
-// JavaScript এ যদি আমারা HTML ট্যাগ এর অ্যাক্সেস পেতে চাই তাহলে  সে ক্ষেত্রে যা ব্যবহার করতে হবে ।
+// ১। JavaScript এ যদি আমারা কোন HTML ট্যাগ এর অ্যাক্সেস পেতে চাই তাহলে  সে ক্ষেত্রে যা ব্যবহার করতে হবে ।
 const tagCollection = document.getElementsByTagName('body');
 console.log(tagCollection);
 
